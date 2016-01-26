0b3cc6585ca431a257714b5d7cfeea64b76bb95f
Brulard <nicolas.brulard@free.fr>
2016-01-26 10:41:39 +0100

    commit changeant le main

--- webserver/main.c
+++ webserver/main.c
@@ -3,11 +3,23 @@
 
 int main ( int argc , char ** argv )
 {
-/* Arnold Robbins in the LJ of February ’95 , describing RCS */
-if ( argc > 1 && strcmp (argv[1],"-advice") == 0) {
-  printf("Don't Panic ! \n");
-  return 42;
-}
-printf("Need an advice ?\n");
-return 0;
+	int socket_serveur;
+
+	if ( argc > 1 && strcmp (argv[1],"-advice") == 0) {
+  		printf("Don't Panic ! \n");
+  		return 42;
+	}
+
+	printf("Need an advice ?\n");
+
+	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_serveur == -1)
+	{
+		perror("socket_serveur");
+		/* traitement de l'erreur */
+	}	
+
+	/* Utilisation de la socket serveur */
+
+	return 0;
 }
