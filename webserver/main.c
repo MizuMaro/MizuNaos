89cc0c01f51df5b08c06faa89d842e41a90a1621
Brulard <nicolas.brulard@free.fr>
2016-01-26 11:22:46 +0100

    commit implementant le bind dans le main

--- webserver/main.c
+++ webserver/main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include <unistd.h>
 #include "prototypes.h"
+#include <arpa/inet.h>
 
 int main ( int argc , char ** argv )
 {
 	int socket_serveur;
+
+	struct sockaddr_in saddr;	
+
+	saddr.sin_family = AF_INET; /* Socket IPv4 */
+	saddr.sin_port = htons(8080); /* Port d'écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
 
 	if ( argc > 1 && strcmp (argv[1],"-advice") == 0) {
   		printf("Don't Panic ! \n");
@@ -20,8 +31,14 @@ int main ( int argc , char ** argv )
 		/* traitement de l'erreur */
 	}
 	printf("Socket_serveur crée !\n");	
-
 	/* Utilisation de la socket serveur */
 
+	if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1)
+	{
+		perror("bind socket_serveur");
+		/* traitement de l'erreur */
+	}
+	
+	
 	return 0;
 }
