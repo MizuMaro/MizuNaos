b7fa4881bab475755ad0a8802dda35fb9d54c1c8
Brulard <nicolas.brulard@free.fr>
2016-01-26 10:46:32 +0100

    commit modifiant le main

--- webserver/main.c
+++ webserver/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+#include "prototypes.h"
 
 int main ( int argc , char ** argv )
 {
@@ -10,14 +12,14 @@ int main ( int argc , char ** argv )
   		return 42;
 	}
 
-	printf("Need an advice ?\n");
-
+	printf("Création de la socket_serveur...\n");
 	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
 	if(socket_serveur == -1)
 	{
 		perror("socket_serveur");
 		/* traitement de l'erreur */
-	}	
+	}
+	printf("Socket_serveur crée !\n");	
 
 	/* Utilisation de la socket serveur */
 
