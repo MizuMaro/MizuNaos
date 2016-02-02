9bf76b45c986b2919ccb2c5360e88a4eee11b83c
Brulard <nicolas.brulard@free.fr>
2016-02-02 09:30:20 +0100

    creation des fichiers socket.h et socket.c

---
+++ webserver/socket.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "socket.h"
+#include <arpa/inet.h>
+
+int creer_serveur(int port)
+{
+
+
+
+
+}
