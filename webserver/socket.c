3f7fe59c26c5a6f86a6f7bff6a42aa311d351a74
MizuMaro <omar.al-yasini@etudiant.univ-lille1.fr>
2016-02-02 09:48:53 +0100

    Mise en place du fichier socket

--- webserver/socket.c
+++ webserver/socket.c
@@ -8,10 +8,25 @@
 #include "socket.h"
 #include <arpa/inet.h>
 
-int creer_serveur(int port)
-{
-
-
-
-
+int creer_serveur(int port){
+	int socketServeur ;
+	socketServeur = socket(AF_INET, SOCK_STREAM, 0);
+	if (socketServeur == -1){
+		perror("socket serveur");
+		return -1;
+	}
+	//Utilisation de la socket serveur
+	struct sockaddr_in socket ;
+	socket.sin_family = AF_INET ; // ipv4
+	socket.sin_port = htons (port); // port ecoute
+	socket.sin_addr.s_addr = INADDR_ANY; 
+	if (bind(socketServeur, (struct sockaddr *)&socket, sizeof(socket)) == -1){
+		perror("erreur bind");
+		return -1;	
+	}
+	if (listen(socketServeur, 10) == -1){
+		perror("erreur listen");
+		return -1;
+	}	
+	return socketServeur;
 }
